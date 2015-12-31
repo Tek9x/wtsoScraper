ff3eb7b409f28609b66e0d06ef6c7faffe4038b4
Tek9x <none@none.com>
2015-12-30 22:16:29 -0500

    Added Shit

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from collections import OrderedDict
 
 class WtsoScraper(object):
     def __init__(self):
-        self.r = requests.get('http://www.wtso.cc/video/vic/eng/season_17?lang=en')
+        self.r = requests.get('http://www.wtso.cc/video/vic/eng/season_23?lang=en')
         self.data = self.r.text
         self.soup = BeautifulSoup(self.data, 'lxml')
 
@@ -45,7 +45,7 @@ class WtsoScraper(object):
     def get_desc(self):
         print '[debug]: Starting [get_desc] function'
         description = []
-        r = requests.get('http://www.imdb.com/title/tt0096697/episodes?season=3')
+        r = requests.get('http://www.imdb.com/title/tt0096697/episodes?season=23')
         data = r.text
         soup = BeautifulSoup(data, 'lxml')
         d = soup.select('div.item_description')
@@ -69,8 +69,8 @@ class WtsoScraper(object):
     def build(self):
         print '[debug]: Starting [building] function'
         #database = zip(self.get_data()[0], self.get_video(), self.get_data()[1], self.get_desc(),self.get_mobile())
-        database = {"Episodes":[{"title":a,"url":b,"thumb":c,"description":d,"mobile":e} for a,b,c,d,e in zip(self.get_data()[0],self.get_video(),self.get_data()[1], self.get_desc(), self.get_mobile())]}
-        save_file(database,'data/Season_3.json')
+        database = {"Episodes":[{"title":a,"url":b,"thumb":c,"mobile":d} for a,b,c,d in zip(self.get_data()[0],self.get_video(),self.get_data()[1], self.get_mobile())]}
+        save_file(database,'data/Season_23.json')
 
 
 abc = WtsoScraper()
